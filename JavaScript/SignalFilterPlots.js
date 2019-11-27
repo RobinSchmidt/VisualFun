8ed6ae8fc5fa38300d1acccf1797a65a6af745bf
Robin Schmidt <robin@rs-met.com>
2019-11-27 09:28:49 +0100

    made grafica deafult plot work

--- JavaScript/SignalFilterPlots.js
+++ JavaScript/SignalFilterPlots.js
@@ -3,14 +3,41 @@ let plotExpDecImpResp = function(p) {
   let y = 100;
 
   p.setup = function() {
-    p.createCanvas(700, 410);
+
+		// Create the canvas
+		var canvas = p.createCanvas(500, 350);
+		p.background(150);
+
+		// Prepare the points for the plot
+		var points = [];
+		var seed = 100 * p.random();
+
+		for (var i = 0; i < 100; i++) {
+			points[i] = new GPoint(i, 10 * p.noise(0.1 * i + seed));
+		}
+
+		// Create a new plot and set its position on the screen
+		var plot = new GPlot(p);
+		plot.setPos(25, 25);
+
+		// Set the plot title and the axis labels
+		plot.setPoints(points);
+		plot.getXAxis().setAxisLabelText("x axis");
+		plot.getYAxis().setAxisLabelText("y axis");
+		plot.setTitleText("A very simple example");
+
+		// Draw it!
+		plot.defaultDraw();
+		p.noLoop();
   };
 
+  /*
   p.draw = function() {
     p.background(0);
     p.fill(255);
     p.rect(x, y, 50, 50);
   };
+  */
 };
 
 //let myp5 = new p5(plotExpDecImpResp);
