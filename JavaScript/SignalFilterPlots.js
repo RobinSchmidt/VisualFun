d93a47ad8b9c15a266a055e660062fe3971e74b7
Robin Schmidt <robin@rs-met.com>
2019-11-27 09:06:50 +0100

    added code to SignalFilterPlots.html/js

---
+++ JavaScript/SignalFilterPlots.js
@@ -0,0 +1,38 @@
+let sketch = function(p) {
+  let x = 100;
+  let y = 100;
+
+  p.setup = function() {
+    p.createCanvas(700, 410);
+  };
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(255);
+    p.rect(x, y, 50, 50);
+  };
+};
+
+let myp5 = new p5(sketch);
+
+
+/*
+function plotExpDecImpResp(p)
+{
+  var canvas = p.createCanvas(500, 350);
+  p.background(150);
+}
+*/
+
+
+/*
+function plot()   // rename to plotExpDecImpResp
+{
+  p = new p5(plotExpDecImpResp, "ExpDecImpResp");
+}
+*/
+
+// see:
+// https://p5js.org/examples/instance-mode-instantiation.html
+
+// later, we want to have several plots on one html page - figure out, how to schieve that
--- ReadMe.md
+++ ReadMe.md
@@ -3,6 +3,7 @@ Visual Fun
 
 Some just-for-fun JavaScript code for creating visual art based on the p5.js library, which is a JavaScript version of the Processing language.
 
-
 https://p5js.org/
 https://processing.org/
+
+...this repo is currently a messy playground for me to learn and experiment with javascript and its libraries. It's not meant for public consumption...not yet, at least...i hope that over time some sort of library for mathematical and artistic visualization will emerge. To this end, i want port some of my C++ math- and signal processing code to JS.
