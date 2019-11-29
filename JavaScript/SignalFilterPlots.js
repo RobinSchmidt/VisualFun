b17770837a4f14851d7eaf0b377744d583892397
Robin Schmidt <robin@rs-met.com>
2019-11-29 04:20:01 +0100

    renamed plotting function

--- JavaScript/SignalFilterPlots.js
+++ JavaScript/SignalFilterPlots.js
@@ -1,4 +1,4 @@
-let plotExpDecImpResp = function(p) {
+let examplePlot = function(p) {
 
   p.setup = function() {
 
@@ -8,9 +8,9 @@ let plotExpDecImpResp = function(p) {
     var margin = 0;
 
 
-    var correction = -100;
-    // for some weird reason, the setDim function of GPlot makes the plots 100 pixels too wide and 
-    // high, so we need this correction -> figure out, what's wrong with grafica here
+    var dimCorrection = -100;
+    // Hack: for some weird reason, the setDim function of GPlot makes the plots 100 pixels too 
+    // wide and high, so we need this correction -> figure out, what's wrong with grafica here
 
     // Create the canvas
     var canvas = p.createCanvas(width, height);
@@ -24,7 +24,7 @@ let plotExpDecImpResp = function(p) {
     // Create a new plot and set its position on the screen
     var plot = new GPlot(p);
     plot.setPos(margin, margin);  // top-left corner
-    plot.setDim([width-margin+correction, height-margin+correction]); 
+    plot.setDim([width-margin+dimCorrection, height-margin+dimCorrection]); 
 
     // Set the plot title and the axis labels
     plot.setPoints(points);
@@ -37,7 +37,6 @@ let plotExpDecImpResp = function(p) {
     p.noLoop();
   };
 
-
   /*
   p.draw = function() {
     p.background(0);
@@ -45,13 +44,16 @@ let plotExpDecImpResp = function(p) {
     p.rect(x, y, 50, 50);
   };
   */
+  // i think, the p.draw function is only needed when we want to periodically redraw the image, 
+  // i.e. create an animation - and it's probably mutually exclusive with calling noLoop?
 };
-
 // it seems, when calling createCanvas multiple times, the canvases are always placed next to 
 // each other and wrapping them into a table doesn't seem to have any effect - it seems, they are
 // not wrapped at all and instead appear next to the table - maybe create canvas always put the
 // canvas(es) into the the top-level html-element - if not told otherwise? maybe we can pass the 
 // containign element -> check docs
+
+
 
 //let myp5 = new p5(plotExpDecImpResp);
 
