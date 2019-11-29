e01ef5f0d9c83ed8b2fa28c290a69f5b0e3c5afe
Robin Schmidt <robin@rs-met.com>
2019-11-29 04:05:08 +0100

    clean up

--- JavaScript/SignalFilterPlots.js
+++ JavaScript/SignalFilterPlots.js
@@ -3,34 +3,38 @@ let plotExpDecImpResp = function(p) {
   p.setup = function() {
 
     // Settings:
-    var width  = 800;
-    var height = 600;
+    var width  = 600;
+    var height = 300;
     var margin = 0;
 
-		// Create the canvas
-		var canvas = p.createCanvas(width, height);
-		p.background(150);
 
-		// Prepare the points for the plot
-		var points = [];
-		for (var i = 0; i < 100; i++)
-      points[i] = new GPoint(i, Math.sin(i/10));
+    var correction = -100;
+    // for some weird reason, the setDim function of GPlot makes the plots 100 pixels too wide and 
+    // high, so we need this correction -> figure out, what's wrong with grafica here
 
-		// Create a new plot and set its position on the screen
-		var plot = new GPlot(p);
-    plot.setPos(margin, margin);                     // top-left corner
-    plot.setDim([width-100, height-100]); // why the fuck do we have to use -100 to make it work?!
-    //plot.setDim([width-2*margin, height-2*margin]);  // width, height
+    // Create the canvas
+    var canvas = p.createCanvas(width, height);
+    p.background(150);
+
+    // Prepare the points for the plot
+    var points = [];
+    for (var i = 0; i < 100; i++)
+      points[i] = new GPoint(i, Math.sin(i / 10));
+
+    // Create a new plot and set its position on the screen
+    var plot = new GPlot(p);
+    plot.setPos(margin, margin);  // top-left corner
+    plot.setDim([width-margin+correction, height-margin+correction]); 
 
     // Set the plot title and the axis labels
-		plot.setPoints(points);
-		plot.getXAxis().setAxisLabelText("x axis");
-		plot.getYAxis().setAxisLabelText("y axis");
-		plot.setTitleText("A very simple example");
+    plot.setPoints(points);
+    plot.getXAxis().setAxisLabelText("x axis");
+    plot.getYAxis().setAxisLabelText("y axis");
+    plot.setTitleText("A very simple example");
 
-		// Draw it!
-		plot.defaultDraw();
-		p.noLoop();
+    // Draw it!
+    plot.defaultDraw();
+    p.noLoop();
   };
 
 
@@ -43,9 +47,11 @@ let plotExpDecImpResp = function(p) {
   */
 };
 
-// it seems, when calling createCanvas multiple times, the canvases are alway paced next tp each other
-// an wrapping them into a table doesn't seem to have any effect - it seems, they are not wrapped at 
-// all and instead appear next to the table
+// it seems, when calling createCanvas multiple times, the canvases are always placed next to 
+// each other and wrapping them into a table doesn't seem to have any effect - it seems, they are
+// not wrapped at all and instead appear next to the table - maybe create canvas always put the
+// canvas(es) into the the top-level html-element - if not told otherwise? maybe we can pass the 
+// containign element -> check docs
 
 //let myp5 = new p5(plotExpDecImpResp);
 
