7b467790fe2adc2bfda9bcead66b917f63117f40
Robin Schmidt <robin@rs-met.com>
2019-11-29 04:35:54 +0100

    added comments

--- JavaScript/SignalFilterPlots.js
+++ JavaScript/SignalFilterPlots.js
@@ -1,4 +1,45 @@
+rsPlotPoints(points, width=600, height=300)
+{
+  var dimCorrection = -100;
+  // Hack: for some weird reason, the setDim function of GPlot makes the plots 100 pixels too 
+  // wide and high, so we need this correction -> figure out, what's wrong with grafica here
+
+  // Create the canvas:
+  var canvas = p.createCanvas(width, height);
+  p.background(150); // maybe use black later
+
+  // Set the plot title and the axis labels:
+  plot.setPoints(points);
+  plot.getXAxis().setAxisLabelText("x axis");
+  plot.getYAxis().setAxisLabelText("y axis");
+  plot.setTitleText("A very simple example");
+
+  // Draw it:
+  plot.defaultDraw();
+  p.noLoop();
+}
+
+rsPlotXY(x, y, width=600, height=300)
+{
+  // todo: check that x and y have the same size
+  var points = [];
+  for (var i = 0; i < x.length; i++)
+    points[i] = new GPoint(x[i], y[i]);
+  rsPlotPoints(points, width, height);
+}
+
+
+
+
+
 let examplePlot = function(p) {
+
+  // what is the argmuent p? in the html-document, we call:
+  // let plot1 = new p5(examplePlot);
+  // is p some p5 object? apparently, as we call p5 functions like createCanvas on it? so how does 
+  // this work? the new p5 object calls a user-supplied function like our examplePlot, passing 
+  // itself als argument? -> look up documentation
+  // we may have to pass this "p" object as additional parameter into our plotting functions
 
   p.setup = function() {
 
@@ -52,6 +93,8 @@ let examplePlot = function(p) {
 // not wrapped at all and instead appear next to the table - maybe create canvas always put the
 // canvas(es) into the the top-level html-element - if not told otherwise? maybe we can pass the 
 // containign element -> check docs
+
+
 
 
 
