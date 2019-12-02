aa17a808c72a44e84a93a1c302b6bd390c1a1c8f
Robin Schmidt <robin@rs-met.com>
2019-12-02 10:24:12 +0100

    moved dat.gui.full.js to Libraries/dat.gui/

--- JavaScript/Libraries/rs/rsPlottingGrafica.js
+++ JavaScript/Libraries/rs/rsPlottingGrafica.js
@@ -1,6 +1,12 @@
-// Plotting functions based on grafica which is a mathematical function plotting library based on 
-// p5.js
+/** Plotting functions based on grafica which is a mathematical function plotting library based on 
+p5.js.
 
-// Dependencies:
-// -grafica.js
-//   -p5.js
+Dependencies:
+-grafica.js
+  -p5.js    */
+
+// ToDo: make a "class" rsPlotter with an interface similar to GNUPlotCPP - later, we may 
+// implement other plotting classes with the same interface but maybe not based on grafica - then
+// we can easily switch between different implementations of the plotting functionality
+
+
