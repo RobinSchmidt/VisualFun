c02aee298ac7e59db789503e1fbaeceb4747270c
Robin Schmidt <robin@rs-met.com>
2019-12-02 10:45:45 +0100

    ExpDecayFilter: moved reset into prototype

--- JavaScript/Libraries/rs/rsSignalFilters.js
+++ JavaScript/Libraries/rs/rsSignalFilters.js
@@ -11,12 +11,6 @@ ExpDecayFilter = function(timeConstant)
 {
   this.tau   = timeConstant;
   this.state = 0;
-  // maybe move to bottom - implementation details should be irrelevant to client code
-
-  this.reset = function()
-  {
-    this.state = 0;
-  }
 }
 
 ExpDecayFilter.prototype.getSample =
@@ -30,13 +24,13 @@ function(x, dt) // x: input value, dt: time-delta between this and previous samp
   this.state = a * x + bdt * this.state;
   return this.state;
 };
-  // maybe this should be put into the prototype
-  // todo: move this into a file rsSignalFilters.js and make a test script and html file that plots
-  // the impulse response with grafica
+// maybe instead of the the update formula with bdt, simply use the naive formula with recomputed b
+// we shall *not* recompute b *and* use the bdt update formula - it's either/or
+// has no normalization - implement with normalization - see c++ implementation of rsNonUniformOnePole
 
-  // maybe instead of the the update formula with bdt, simply use the naive formula with recomputed b
-  // we shall *not* recompute b *and* use the bdt update formula - it's either/or
-  // has no normalization - implement with normalization - see c++ implementation of rsNonUniformOnePole
+ExpDecayFilter.prototype.reset = function() { this.state = 0; };
+
+
 
 
 // todo make a file rsSignalOscillators/Sources
