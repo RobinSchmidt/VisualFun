2a1fbde9f5dab1330061a6f85363dbcfae02ead5
Robin Schmidt <robin@rs-met.com>
2019-12-02 10:43:28 +0100

    ExpDecayFilter: moved getSample into prototype

--- JavaScript/Libraries/rs/rsSignalFilters.js
+++ JavaScript/Libraries/rs/rsSignalFilters.js
@@ -13,18 +13,23 @@ ExpDecayFilter = function(timeConstant)
   this.state = 0;
   // maybe move to bottom - implementation details should be irrelevant to client code
 
-  this.getSample = function(x, dt) // x: input value, dt: time-delta between this and previous sample
+  this.reset = function()
   {
-    if(dt <= 0) // sanity check - we are a bit defensive here
-      return x;  
-    //var b      = exp(-dt / this.tau);  // verify this formula - decay seems too slow
-    var b      = Math.exp(-1 / this.tau);
-    //b = 0.25; //test
-    var a      = 1 - b;
-    var bdt    = Math.pow(b, dt);
-    this.state = a * x + bdt * this.state;
-    return this.state;
+    this.state = 0;
   }
+}
+
+ExpDecayFilter.prototype.getSample =
+function(x, dt) // x: input value, dt: time-delta between this and previous sample
+{
+  if(dt <= 0) // sanity check - we are a bit defensive here
+    return x;  
+  var b = Math.exp(-1 / this.tau); // verify this formula - decay seems too slow
+  var a = 1 - b;
+  var bdt = Math.pow(b, dt);
+  this.state = a * x + bdt * this.state;
+  return this.state;
+};
   // maybe this should be put into the prototype
   // todo: move this into a file rsSignalFilters.js and make a test script and html file that plots
   // the impulse response with grafica
@@ -32,12 +37,6 @@ ExpDecayFilter = function(timeConstant)
   // maybe instead of the the update formula with bdt, simply use the naive formula with recomputed b
   // we shall *not* recompute b *and* use the bdt update formula - it's either/or
   // has no normalization - implement with normalization - see c++ implementation of rsNonUniformOnePole
-
-  this.reset = function()
-  {
-    this.state = 0;
-  }
-}
 
 
 // todo make a file rsSignalOscillators/Sources
